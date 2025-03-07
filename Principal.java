2be5bcdec04b6c56cab71bc652cb993519bb8cb9
Gerardo González Martínez <136201301+gerl0143@users.noreply.github.com>
2025-03-07 14:05:01 -0600

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,19 @@
+package miniproyecto;
+import interfaz.Pantalla;
+/**
+ *
+ * @author gerar
+ */
+public class Miniproyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Pantalla ventana=new Pantalla();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+    
+}
